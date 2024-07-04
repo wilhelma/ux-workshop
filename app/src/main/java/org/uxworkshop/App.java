6ec8169fe88a217732bd88d7b505ce8b4aba9436
Andreas Wilhelm <wilhelm@cqse.eu>
2024-07-04 11:25:33 +0200

    add finding

--- app/src/main/java/org/uxworkshop/App.java
+++ app/src/main/java/org/uxworkshop/App.java
@@ -5,7 +5,7 @@ package org.uxworkshop;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello World";
     }
 
     public static void main(String[] args) {
